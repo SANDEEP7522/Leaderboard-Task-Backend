1f4611e169dc5ccb0b73a5afb319dba58ca132f8
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-08-19 19:13:26 +0530

    Added ..

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -13,3 +13,5 @@ export const PROD_DB_URL = process.env.PROD_DB_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
 
 export const JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
+
+export const FRONTEND_URL = process.env.FRONTEND_URL;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import cors from 'cors';
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
-import { PORT } from './config/serverConfig.js';
+import { FRONTEND_URL, PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
